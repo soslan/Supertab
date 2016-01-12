97eba48e121e023420b5b4a81d38348e13fd776b
Soslan Khubulov <soslanx@gmail.com>
2016-01-12 14:26:08 +0300

    Fix icon building

--- build.sh
+++ build.sh
@@ -11,12 +11,16 @@ cp manifest.json newtab.html supertab.css supertab.js background.js ../$DIST_DIR
 cd ../
 
 function build_icons {
-convert -resize 19x19 -background none src/$1.svg $DIST_IMG/$1_19x19.png
-convert -resize 38x38 -background none src/$1.svg $DIST_IMG/$1_38x38.png
+convert -resize 19x19 -background none src/icons/$1.svg $DIST_IMG/$1_19x19.png
+convert -resize 38x38 -background none src/icons/$1.svg $DIST_IMG/$1_38x38.png
 }
 
 build_icons push
-build_icons icon
 build_icons remove
 build_icons radio_enabled
 build_icons radio_disabled
+
+convert -resize 19x19 -background none src/icons/icon.svg $DIST_IMG/icon_16x16.png
+convert -resize 38x38 -background none src/icons/icon.svg $DIST_IMG/icon_32x32.png
+convert -resize 19x19 -background none src/icons/icon.svg $DIST_IMG/icon_48x48.png
+convert -resize 38x38 -background none src/icons/icon.svg $DIST_IMG/icon_128x128.png
--- build_devel.sh
+++ build_devel.sh
@@ -16,10 +16,14 @@ convert -resize 38x38 -background none src/icons/$1.svg $DIST_IMG/$1_38x38.png
 }
 mkdir -p src/icons
 build_icons push
-build_icons icon
 build_icons remove
 build_icons radio_enabled
 build_icons radio_disabled
+
+convert -resize 19x19 -background none src/icons/icon.svg $DIST_IMG/icon_16x16.png
+convert -resize 38x38 -background none src/icons/icon.svg $DIST_IMG/icon_32x32.png
+convert -resize 19x19 -background none src/icons/icon.svg $DIST_IMG/icon_48x48.png
+convert -resize 38x38 -background none src/icons/icon.svg $DIST_IMG/icon_128x128.png
 
 while true; do
   change=$(inotifywait -e modify src/icons/)
