d8f6db233c151571c1d4bdfafbf7d648823dfc7a
Soslan Khubulov <soslanx@gmail.com>
2016-02-09 15:28:41 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-DIST_DIR=dist/
-DIST_IMG=$DIST_DIR/img/
 
-rm -rf $DIST_DIR/*
+VERSION=`python -c 'import json;print(json.load(open("src/manifest.json"))["version"])'`
+NAME=Supertab-$VERSION
+DIST_DIR=dist/
+DEST=$DIST_DIR/$NAME
+DIST_IMG=$DEST/img/
+rm -rf $DEST/*
 mkdir -p $DIST_IMG
+mkdir -p $DIST_DIR/promo
 
 cd src/
-cp manifest.json newtab.html supertab.css supertab.js background.js ../$DIST_DIR
+cp manifest.json newtab.html supertab.css supertab.js background.js ../$DEST
 cd ../
 
 function build_icons {
@@ -24,6 +28,10 @@ convert -resize 32x32 -background none src/icons/icon.svg $DIST_IMG/icon_32x32.p
 convert -resize 48x48 -background none src/icons/icon.svg $DIST_IMG/icon_48x48.png
 convert -resize 128x128 -background none src/icons/icon.svg $DIST_IMG/icon_128x128.png
 
+convert -background none src/images/promo_440x280.svg $DIST_DIR/promo/promo_440x280.png
+convert -background none src/images/promo_920x680.svg $DIST_DIR/promo/promo_920x680.png
+convert -background none src/images/promo_1400x560.svg $DIST_DIR/promo/promo_1400x560.png
+
 cd dist/
-zip Supertab *
+zip -r $NAME.zip $NAME/*
 cd ../
